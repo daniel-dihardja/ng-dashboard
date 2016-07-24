f3262ac25ba270ee6407519c13e2582dc4a56f6e
Daniel Dihardja <daniel@dihardja.de>
2016-07-24 19:49:14 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+/**
+ * Created by danieldihardja on 24/07/16.
+ */
+
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+	browserSync.init({
+		server: {
+			baseDir: "./src",
+			// serve our jspm dependencies with the client folder
+			routes: {
+				'/jspm.config.js': './jspm.config.js'
+			}
+		}
+	});
+
+	gulp.watch("src/**/*.scss", ['sass']);
+	gulp.watch("src/*.html").on('change', browserSync.reload);
+	gulp.watch("src/**/*.js").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+	return gulp.src("src/sass/main.scss")
+		.pipe(sass())
+		.pipe(gulp.dest("src/css"))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('css', ['sass'], function() {
+	gulp.src('./src/css/main.css')
+		.pipe(gulp.dest('./dist'));
+});
+
+
+gulp.task('default', ['serve']);
---
+++ src/main.js
@@ -0,0 +1,4 @@
+/**
+ * Created by danieldihardja on 24/07/16.
+ */
+import angular from 'angular';
