b4c3c9d658686e436ac450f37b06df9e6967b534
Daniel Dihardja <daniel@dihardja.de>
2016-09-27 13:05:04 +0200

    added text file

--- src/app/app.text.js
+++ src/app/app.text.js
@@ -1,39 +1,11 @@
 /**
  * Created by danieldihardja on 18/08/16.
  */
+import cmsTxt from './zfcms-text';
+
 function appText($translateProvider) {
 
-	$translateProvider.translations('de', {
-
-		app: {
-			title:	'ZF Admin',
-			yes:	'Ja',
-			no:		'Nein',
-			delete:	'Löschen',
-			logout: 'Logout'
-		},
-
-		login: {
-			title: 		'ZF Login',
-			username: 	'Username',
-			password: 	'Password',
-			submit: 	'Login'
-		},
-
-		files: {
-			title: 		'Dateien',
-			new: 		'Neu',
-			upload: 	'Hochladen',
-			delete: 	'Löschen',
-			list: 		'Liste',
-			images:		'Bilder',
-			videos:		'Videos'
-		},
-
-		profile: {
-			title: 		'Profil'
-		}
-	});
+	$translateProvider.translations('de', cmsTxt);
 
 	$translateProvider.preferredLanguage('de');
 }
---
+++ src/app/zfcms-text.js
@@ -0,0 +1,18 @@
+/**
+ * Created by danieldihardja on 27/09/16.
+ */
+export default {
+	txt: {
+		login: 		'Login',
+		username: 	'Username',
+		password: 	'Password',
+		cmsTitle:	'ZF Forum',
+
+		create: 	'Create',
+		edit:		'Edit',
+		list: 		'List',
+
+		toMedia:	'zu den Medien'
+
+	}
+};
